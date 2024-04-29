8f84d0476ac05e248565fff31b47811dc9fb34c4
Samuel Escobar <samueb8@gmail.com>
2024-04-29 08:19:35 -0400

    agregar las librerias de firebase

---
+++ src/app/services/FirebaseService.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, get } from "firebase/database";
+import {getStorage, ref as refStorage, getDownloadURL} from "firebase/storage";
+
+// Configuración de Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCiXm7xzyou9GySZ7Y7x1tmm6CYs-BBars",
+    authDomain: "club-social-petrolero.firebaseapp.com",
+    databaseURL: "https://club-social-petrolero-default-rtdb.firebaseio.com",
+    projectId: "club-social-petrolero",
+    storageBucket: "club-social-petrolero.appspot.com",
+    messagingSenderId: "95503166689",
+    appId: "1:95503166689:web:e8ba558c1aa1daebf25ba0",
+    measurementId: "G-G9RMY72TFT"
+  };
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const storage = getStorage();
