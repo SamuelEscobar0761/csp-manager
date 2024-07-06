301aaeeb1a519d5b77ebc3694b25fb4723f39c32
Samuel Escobar <samueb8@gmail.com>
2024-07-06 18:00:03 -0400

    trabajando en el frontend de los pdfs

--- src/app/components/AdminSidebar.tsx
+++ src/app/components/AdminSidebar.tsx
@@ -24,70 +24,70 @@ const AdminSidebar = ({ setPage, setComponent }: { setPage: any; setComponent: a
       <h2 className="p-5 font-bold text-lg">Administración</h2>
       <NavbarItem title="Inicio">
         <div className="py-1 hover:bg-gray-600">
-          <Link to='/' onClick={() => { setPage("homepage"); setComponent("information") }}>Foto de información general del Club</Link>
+          <Link to='/' className='block w-full' onClick={() => { setPage("homepage"); setComponent("information") }}>Foto de información general del Club</Link>
         </div>
       </NavbarItem>
       <NavbarItem title="Quiénes somos?">
         <div className="py-1 hover:bg-gray-600">
-          <Link to='/' onClick={() => { setPage("about_us_page"); setComponent("directory") }}>Directorio</Link>
+          <Link to='/' className='block w-full' onClick={() => { setPage("about_us_page"); setComponent("directory") }}>Directorio</Link>
         </div>
         <div className="py-1 hover:bg-gray-600">
-          <Link to='/about_us' onClick={() => { setPage("about_us_page"); setComponent("statute") }}>Estatuto</Link>
+          <Link to='/about_us' className='block w-full' onClick={() => { setPage("about_us_page"); setComponent("statute") }}>Estatuto</Link>
         </div>
         <div className="py-1 hover:bg-gray-600">
-          <Link to='/regulations' onClick={() => { setPage("about_us_page"); setComponent("regulations") }}>Reglamentos</Link>
+          <Link to='/regulations' className='block w-full' onClick={() => { setPage("about_us_page"); setComponent("regulations") }}>Reglamentos</Link>
         </div>
         <div className="py-1 hover:bg-gray-600">
-          <Link to='/about_us' onClick={() => { setPage("about_us_page"); setComponent("memories") }}>Memorias</Link>
+          <Link to='/about_us' className='block w-full' onClick={() => { setPage("about_us_page"); setComponent("memories") }}>Memorias</Link>
         </div>
       </NavbarItem>
       <NavbarItem title="Deportes">
         <div className="py-1 hover:bg-gray-600">
-          <Link to='/' onClick={() => { setPage("swimming_page"); setComponent("information") }}>Natación</Link>
+          <Link to='/' className='block w-full' onClick={() => { setPage("swimming_page"); setComponent("information") }}>Natación</Link>
         </div>
         <div className="py-1 hover:bg-gray-600">
-          <Link to='/' onClick={() => { setPage("tennis_page"); setComponent("information") }}>Tenis</Link>
+          <Link to='/' className='block w-full' onClick={() => { setPage("tennis_page"); setComponent("information") }}>Tenis</Link>
         </div>
         <div className="py-1 hover:bg-gray-600">
-          <Link to='/' onClick={() => { setPage("racket_page"); setComponent("information") }}>Racquet</Link>
+          <Link to='/' className='block w-full' onClick={() => { setPage("racket_page"); setComponent("information") }}>Racquet</Link>
         </div>
         <div className="py-1 hover:bg-gray-600">
-          <Link to='/' onClick={() => { setPage("paddle_page"); setComponent("information") }}>Paleta</Link>
+          <Link to='/' className='block w-full' onClick={() => { setPage("paddle_page"); setComponent("information") }}>Paleta</Link>
         </div>
         <div className="py-1 hover:bg-gray-600">
-          <Link to='/' onClick={() => { setPage("football_page"); setComponent("information") }}>Fútbol</Link>
+          <Link to='/' className='block w-full' onClick={() => { setPage("football_page"); setComponent("information") }}>Fútbol</Link>
         </div>
         <div className="py-1 hover:bg-gray-600">
-          <Link to='/' onClick={() => { setPage("gym_page"); setComponent("information") }}>Gimnasio</Link>
+          <Link to='/' className='block w-full' onClick={() => { setPage("gym_page"); setComponent("information") }}>Gimnasio</Link>
         </div>
         <div className="py-1 hover:bg-gray-600">
-          <Link to='/' onClick={() => { setPage("cycling_page"); setComponent("information") }}>Ciclismo</Link>
+          <Link to='/' className='block w-full' onClick={() => { setPage("cycling_page"); setComponent("information") }}>Ciclismo</Link>
         </div>
         <div className="py-1 hover:bg-gray-600">
-          <Link to='/' onClick={() => { setPage("wally_page"); setComponent("information") }}>Wally</Link>
+          <Link to='/' className='block w-full' onClick={() => { setPage("wally_page"); setComponent("information") }}>Wally</Link>
         </div>
       </NavbarItem>
       <NavbarItem title="Restaurante y Snack">
         <div className="py-1 hover:bg-gray-600">
-          <Link to='/' onClick={() => { setPage("restaurant_page"); setComponent("weekly_menu") }}>Menú Semanal</Link>
+          <Link to='/' className='block w-full' onClick={() => { setPage("restaurant_page"); setComponent("weekly_menu") }}>Menú Semanal</Link>
         </div>
         <div className="py-1 hover:bg-gray-600">
-          <Link to='/' onClick={() => { setPage("restaurant_page"); setComponent("main_menu") }}>Menú Comedor</Link>
+          <Link to='/' className='block w-full' onClick={() => { setPage("restaurant_page"); setComponent("main_menu") }}>Menú Comedor</Link>
         </div>
         <div className="py-1 hover:bg-gray-600">
-          <Link to='/' onClick={() => { setPage("restaurant_page"); setComponent("snack_menu") }}>Menú Snack</Link>
+          <Link to='/' className='block w-full' onClick={() => { setPage("restaurant_page"); setComponent("snack_menu") }}>Menú Snack</Link>
         </div>
       </NavbarItem>
       <NavbarItem title="Únete al Club">
         <div className="py-1 hover:bg-gray-600">
-          <Link to='/' onClick={() => { setPage("join_us_page"); setComponent("tariff") }}>Tarifario</Link>
+          <Link to='/' className='block w-full' onClick={() => { setPage("join_us_page"); setComponent("tariff") }}>Tarifario</Link>
         </div>
         <div className="py-1 hover:bg-gray-600">
-          <Link to='/' onClick={() => { setPage("join_us_page"); setComponent("participation") }}>Participación</Link>
+          <Link to='/' className='block w-full' onClick={() => { setPage("join_us_page"); setComponent("participation") }}>Participación</Link>
         </div>
       </NavbarItem>
       <div className="pl-4 py-1 hover:bg-gray-600">
-        <Link to='/news' onClick={() => { setPage("news"); setComponent("news") }}>Noticias</Link>
+        <Link to='/news' className='block w-full' onClick={() => { setPage("news"); setComponent("news") }}>Noticias</Link>
       </div>
     </div>
   );
--- src/app/pages/AboutUsPage.tsx
+++ src/app/pages/AboutUsPage.tsx
@@ -31,11 +31,32 @@ export const AboutUsPage = ({ page, component }: { page: string, component: stri
 
     if(component === 'regulations'){
         return(
-            <></>
+            <div>
+            
+            </div>
         );
     }
 
     return(
-        <></>
-    );    
+        <div className="w-full py-5">
+            {pdfInformationObjects.map((item, index) =>(
+              <div className="h-screen flex justify-start ..." key={index}>
+                <iframe src={item.url!} className='pl-20 pr-5 w-2/3 h-full'/>
+                <div className="bg-gray-200 w-3/12 h-1/6 rounded">
+                  <h2 className="text-5xl pl-5 pt-2">Estatuto</h2>
+                  <div className="m-5">
+                    <p className="mr-2 text-xl">Nombre del documento: {item.name}</p>
+                  </div>
+                  <div className="m-5 flex justify-end ...">
+                    <button onClick={() => {}} className="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Editar</button>
+                    <button onClick={() => {}} className="m-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Borrar</button>
+                  </div>
+                </div>
+              </div>
+            ))}
+            <button onClick={()=>{}} className="fixed bottom-4 right-4 bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded inline-flex items-center">
+                <span className="text-xl">+</span>
+            </button>
+        </div>
+    );  
 }
