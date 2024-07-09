1bbf1d74fc7df50eee007b8a4dbeb4dd03390505
Samuel Escobar <samueb8@gmail.com>
2024-07-08 22:52:54 -0400

    Pagina de reglamentos terminada el front-end

---
+++ src/app/components/RegulationCard.tsx
@@ -0,0 +1,23 @@
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+
+export const RegulationCard = ({ key, name, url, onEdit, onDelete }: { key: string, name: string, url: string, onEdit: any, onDelete: any }) => {
+    return(
+    <div className="bg-gray-100 p-5 rounded">
+      <div className="flex h-full pb-5 items-center justify-start ... ">
+        <div className='pt-5'>
+            <img src='/pdf_icon.png' className="w-20 h-20"/>
+        </div>
+        <div className="flex-1 ml-4">
+          <div className="flex mb-2 justify-end ...">
+            <button onClick={() => {onEdit()}} className="mx-5 text-white bg-blue-500 rounded p-2 w-10 h-10"><EditOutlinedIcon/></button>
+            <button onClick={() => {onDelete()}} className="text-white bg-red-500 rounded py-2 w-10 h-10"><DeleteOutlineOutlinedIcon/></button>
+          </div>
+            <a href={url} className="break-words underline"> {/* break-words permite que el texto se ajuste y rompa correctamente */}
+              {name}
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+}
--- src/app/pages/RegulationsPage.tsx
+++ src/app/pages/RegulationsPage.tsx
@@ -1,3 +1,109 @@
+import { useCallback, useEffect, useState } from "react";
+import InformationObject from "../interfaces/InformationObject";
+import { addInformationObject, deleteInformationObject, editInformationObject, getInformationObjects } from "../services/FirebaseService";
+import { RegulationCard } from "../components/RegulationCard";
+
 export const RegulationsPage = ({ page, component }: { page: string, component: string }) => {
-    return <></>
+    const [loading, setLoading] = useState<boolean>(false);
+    const [pdfInformationObjects, setPdfInformationObjects] = useState<InformationObject[]>([]);
+    const [editDialog, setEditDialog] = useState<{ show: boolean, pdfKey?: string, pdfName?: string }>({ show: false });
+    const [confirmButtonEnable, setConfirmButtonEnable] = useState<boolean>(false);
+    const [newPdf, setNewPdf] = useState<{ name?: string | null, file?: File | null}>({name: ""});
+    const [confirmDelete, setConfirmDelete] = useState<{ show: boolean, pdfKey?: string }>({ show: false });
+    const [showAddDialog, setShowAddDialog] = useState(false);
+    
+    // Encapsula la función de carga en useCallback para evitar redefiniciones innecesarias
+    const loadPdfs = useCallback(async () => {
+        setLoading(true);
+        try {
+            const infoPdfs = await getInformationObjects('pdfs', page, component);
+            setPdfInformationObjects(infoPdfs);
+        } catch (error) {
+            console.error('Error al obtener las imágenes:', error);
+        } finally {
+            setLoading(false);
+        }
+    }, [page, component]);
+
+    const handleEditClick = async (key: string, pdfName: string) => {
+        setConfirmButtonEnable(true);
+        setEditDialog({ show: true, pdfKey: key, pdfName: pdfName });
+        setNewPdf({name: pdfName});
+    };
+
+    const handleEditSubmit = async () => {
+        if (editDialog.pdfKey && confirmButtonEnable) {
+            setConfirmButtonEnable(false);
+            const response = await editInformationObject('pdfs', editDialog.pdfKey, newPdf.file? newPdf.file : null, newPdf.name!);
+            if (response.success) {
+                setEditDialog({ show: false });
+                await loadPdfs(); // Reload the images to reflect the changes
+            } else {
+                alert('Error al subir el archivo pdf, error de conexión');
+            }
+        }
+    };
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files ? event.target.files[0] : null;
+        setNewPdf(prev => ({...prev, file: file}));
+    };
+
+    const cancelEdit = () => {
+        setEditDialog({ show: false });
+        setNewPdf({name: editDialog.pdfName, file: null});
+    };
+
+    const handleDeleteClick = (pdfKey: string) => {
+        setConfirmButtonEnable(true);
+        setConfirmDelete({ show: true, pdfKey: pdfKey });
+    };
+
+    const handleDeleteConfirm = async () => {
+        if (confirmDelete.pdfKey && confirmButtonEnable) {
+            setConfirmButtonEnable(false);
+            const result = await deleteInformationObject('pdfs', confirmDelete.pdfKey);
+            if (result.success) {
+                setConfirmDelete({ show: false });
+                loadPdfs();  // Recargar la lista de PDFs
+            } else {
+                alert(result.message);
+            }
+        }
+    };
+
+    // Función para manejar la subida de un nuevo PDF
+    const handleNewPdfSubmit = async () => {
+        if (newPdf.file && newPdf.name && confirmButtonEnable) {
+            setConfirmButtonEnable(false);
+            const result = await addInformationObject('pdfs', newPdf.file, component, newPdf.name, page);
+            if (result.success) {
+                setShowAddDialog(false);
+                loadPdfs(); // Recargar la lista de PDFs
+            } else {
+                alert('Please ensure both file and name are provided: ' + result.message);
+            }
+        }
+    };
+
+    // Cargar imágenes al montar y cuando cambian las dependencias
+    useEffect(() => {
+        loadPdfs();
+    }, [loadPdfs]);
+
+    if (loading) {
+        return <div><p>Cargando...</p></div>;
+    }
+    
+    return(
+        <div>
+            <h2 className="px-5 py-10 text-7xl">Reglamentos</h2>
+            <div className="grid grid-cols-3 gap-5 auto-rows-min px-5">
+                {pdfInformationObjects.map((item, index) =>(
+                    <RegulationCard key={item.key} name={item.name} url={item.url!} onEdit={() => handleEditClick(item.key, item.name)} onDelete={() => handleDeleteClick(item.key)}/>
+                ))}
+            </div>
+        </div>
+    
+    );
 }
